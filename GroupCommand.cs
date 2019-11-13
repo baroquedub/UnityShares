936551307df7c9438f12e0107b11aba9c42ead83
baroquedub <baroquedub@googlemail.com>
2019-11-13 13:42:09 +0000

    Add files via upload

---
+++ GroupCommand.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public static class GroupCommand
+{
+	[MenuItem("GameObject/Group Selected %q")]
+	private static void GroupSelected()
+	{
+		if (!Selection.activeTransform) return;
+		var go = new GameObject("Small_" + Selection.activeTransform.name + " Group");
+		Undo.RegisterCreatedObjectUndo(go, "Group Selected");
+		go.transform.SetParent(Selection.activeTransform.parent, false);
+		foreach (var transform in Selection.transforms) Undo.SetTransformParent(transform, go.transform, "Group Selected");
+		Selection.activeGameObject = go;
+	}
+}
