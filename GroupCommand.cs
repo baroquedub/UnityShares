7d45dc83febbd49cf64b2ed5711e9d5d31fbb649
baroquedub <baroquedub@googlemail.com>
2020-02-03 22:03:31 +0000

    Simplified naming of new group

--- GroupCommand.cs
+++ GroupCommand.cs
@@ -7,7 +7,7 @@ public static class GroupCommand
 	private static void GroupSelected()
 	{
 		if (!Selection.activeTransform) return;
-		var go = new GameObject("GROUP_" + Selection.activeTransform.name + " Group");
+		var go = new GameObject("_" + Selection.activeTransform.name + " Group");
 		Undo.RegisterCreatedObjectUndo(go, "Group Selected");
 		go.transform.SetParent(Selection.activeTransform.parent, false);
 		foreach (var transform in Selection.transforms) Undo.SetTransformParent(transform, go.transform, "Group Selected");
